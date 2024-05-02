9881ceab67409902b6d2f9f04e2dabd310a14b2a
crzqj <realunix@163.com>
2024-05-02 17:13:32 +0800

    123

--- src/hooks/useURLLoader.ts
+++ src/hooks/useURLLoader.ts
@@ -1,29 +1,6 @@
 import { ref } from 'vue'
 import axios from 'axios'
 
-// function useURLLoader<T>(url: string) {
-//     const result = ref<T | null>(null)
-//     const loading = ref(true)
-//     const loaded = ref(false)
-//     const error = ref(null)
-
-//     axios.get(url).then((rawData) => {
-//         loading.value = false
-//         loaded.value = true
-//         result.value = rawData.data
-//     }).catch(e => {
-//         error.value = e
-//         loading.value = false
-//     })
-
-//     return {
-//         result,
-//         loading,
-//         error,
-//         loaded
-//     }
-// }
-
 export default function useURLLoader<T>(url: string) {
     const result = ref<T | null>(null);
     const loading = ref(false);
@@ -45,4 +22,4 @@ export default function useURLLoader<T>(url: string) {
     fetchData(); // Fetch data when hook is first called
   
     return { result, loading, loaded, error, fetchData };
-  }
+}
