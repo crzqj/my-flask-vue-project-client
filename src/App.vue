ebd0ab807a571fa32807a8620c19e44356fb60a5
crzqj <realunix@163.com>
2024-05-03 21:53:24 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,11 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
-
-<!-- <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script> -->
 
 <style>
 #app {
-  margin-top: 60px
+  margin-top: 60px;
 }
 </style>
--- src/hooks/useURLLoader.ts
+++ src/hooks/useURLLoader.ts
@@ -1,25 +1,25 @@
-import { ref } from 'vue'
-import axios from 'axios'
+import { ref } from "vue";
+import axios from "axios";
 
 export default function useURLLoader<T>(url: string) {
-    const result = ref<T | null>(null);
-    const loading = ref(false);
-    const loaded = ref(false);
-    const error = ref<unknown>(null); // 修改此行
-  
-    const fetchData = async () => {
-      loading.value = true;
-      try {
-        const response = await axios.get(url);
-        result.value = response.data;
-        loaded.value = true;
-      } catch (err) {
-        error.value = err; // 修改此行
-      }
-      loading.value = false;
-    };
-  
-    fetchData(); // Fetch data when hook is first called
-  
-    return { result, loading, loaded, error, fetchData };
+  const result = ref<T | null>(null);
+  const loading = ref(false);
+  const loaded = ref(false);
+  const error = ref<unknown>(null); // 修改此行
+
+  const fetchData = async () => {
+    loading.value = true;
+    try {
+      const response = await axios.get(url);
+      result.value = response.data;
+      loaded.value = true;
+    } catch (err) {
+      error.value = err; // 修改此行
+    }
+    loading.value = false;
+  };
+
+  fetchData(); // Fetch data when hook is first called
+
+  return { result, loading, loaded, error, fetchData };
 }
